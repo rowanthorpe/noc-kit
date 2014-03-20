8f7fa0b9cad88de3ccc7ff411f561346946cffa0
Rowan Thorpe <rowan@rowanthorpe.com>
2014-03-20 20:31:36 +0200

    Try again to get travis directory-stuff working

--- tests/travis.sh
+++ tests/travis.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd "`dirname "\`readlink -e "$0"\`"`/.." && \
+cd "$(dirname "$(readlink -e "$0")")/.." && \
 env CFLAGS="-D_POSIX_C_SOURCE=200809L -Wall -Wextra -pedantic-errors -Wno-overlength-strings -static -fomit-frame-pointer -std=c99 -O3 -march=native" ./configure && \
 make && \
 sudo make install && \
