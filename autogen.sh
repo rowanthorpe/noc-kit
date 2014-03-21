ced6f7a86b90323f3f3a546194baf49b603ba376
Rowan Thorpe <rowan@rowanthorpe.com>
2014-03-21 11:18:29 +0200

    Make default autogen simpler,add marker to keep m4 dir

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-autoreconf --verbose --install -I config -I m4 --warnings=all || exit 1
+autoreconf --verbose --install --warnings=all || exit 1
+## Maintainer version ;-)  (upgrades autotools, sanity-checks the build)
+#autoreconf --verbose --install --warnings=all --make --force || exit 1
